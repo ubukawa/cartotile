1ab44c0b43f4048aeac17ce879b45c74d6dfc9c4
ubukawa <ubukawa-t96dq@mlit.go.jp>
2022-12-08 09:47:20 -0500

    rev

--- test01.js
+++ test01.js
@@ -1,13 +1,16 @@
 //modules
 const fetch = require('node-fetch')
 const config = require('config')
+const fs = require('fs')
 
 //parameters
 const featureServer = config.get('url')
 const layers = config.get('layers')
 const layerId = config.get('layerId')
 const layerRecord = config.get('layerRecord')
-var data = []
+const outputText = config.get('outputText')
+//var data = []
+const stream = fs.createWriteStream(outputText)
 
 //actual code
 async function get_feature(url,layerName3){
@@ -37,25 +40,50 @@ async function get_feature(url,layerName3){
     delete f.properties.globalid  //delete unnnecesary attribution
     delete f.properties.globalid_1   //delete unnnecesary attribution
     delete f.properties.SHAPE__Length   //delete unnnecesary attribution
-    data.push(f)
-//    console.log(data)
+//    data.push(f)
+    stream.write(JSON.stringify(f))
+    stream.write(', \n')
 }
 
-
 async function getlayer(layer,count,layerName2){
+    console.log('Starting the work!!!!!!')
+    console.log(Date())
+    console.log('----->')
     for (var i = 1; i < count + 1; i ++){
         var featureUrl = featureServer + layer + '/' + i + '?f=pjson'
         await get_feature(featureUrl,layerName2)
     }
 }
 
-for (let layer in layers){
-    let num = layerId[layers[layer]]
-    let record = layerRecord[layers[layer]]
-    let layerName1 = layers[layer]
-    getlayer(num,record,layerName1).then(()=>{
-        console.log(JSON.parse(data))
-    })
+//console.log(layerId[layers[0]])  //like "0" or "3"
+//console.log(layerRecord[layers[0]]) //like "246" or "742"
+//console.log(layers[0]) //like "bnda" or "bndl"
+//getlayer(layerId[layers[0]],layerRecord[layers[0]],layers[0]).then(()=>{
+getlayer(layerId[layers[0]],10,layers[0]).then(()=>{
+//    console.log(JSON.parse(data[0]))
+//    console.log(data[0])
+      stream.end()
+      console.log('end!!! Bye-bye')
+      console.log(Date())
+})
+
+
+/*
+async function main(){
+    for (let layer in layers){
+        let num = layerId[layers[layer]]
+        let record = layerRecord[layers[layer]]
+        let layerName1 = layers[layer]
+        getlayer(num,10,layerName1).then(()=>{
+ //       getlayer(num,record,layerName1).then(()=>{
+        })
+    }
 }
 
+main().then(()=>{
+    stream.end()
+    console.log('end!!! Bye-bye')
+    console.log(Date())
+})
+*/
 
